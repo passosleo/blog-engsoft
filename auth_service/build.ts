6f19fdf6b709712d18596f8b295c93ca7d994bac
passosleo <leonardopassos98@gmail.com>
2024-04-13 23:16:22 -0300

    fix: build pipeline

--- auth_service/build.ts
+++ auth_service/build.ts
@@ -1,9 +1,6 @@
 import fs from 'fs-extra';
 import packageJson from './package.json';
 
-fs.copySync('src/app/views', 'dist/src/app/views');
-fs.copySync('public', 'dist/public');
-fs.mkdirpSync('dist/uploads/avatars');
 fs.mkdirpSync('dist/prisma');
 fs.copySync('prisma/schema.prisma', 'dist/prisma/schema.prisma');
 fs.copySync('Dockerfile', 'dist/Dockerfile');
