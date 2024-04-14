9b80d8f5d8487f00b220f4dc20e1647353415609
rayane.melo <rayane.melo@corebiz.ag>
2024-04-14 00:17:06 -0300

    feat: styles posts

--- frontend/src/components/Posts/Post/index.tsx
+++ frontend/src/components/Posts/Post/index.tsx
@@ -1,5 +1,7 @@
 import ReactHtmlParser from "react-html-parser";
 import "./global.css";
+import { format, formatDistanceToNow } from "date-fns";
+import ptBR from "date-fns/locale/pt-BR";
 
 type Props = {
   post: {
@@ -8,14 +10,45 @@ type Props = {
     content: string;
     author: string;
     publishedAt: Date;
-  }
-}
+  };
+};
 
-export function Post({post}: Props){
+export function Post({ post }: Props) {
+  const publishedDateFormatted = format(
+    post.publishedAt || new Date(),
+    "dd 'de' MMMM 'de' yyyy 'às' HH:mm",
+    { locale: ptBR }
+  );
+
+  const publishedDateRelativeToNow = formatDistanceToNow(
+    post.publishedAt || new Date(),
+    {
+      locale: ptBR,
+      addSuffix: true,
+    }
+  );
+
   return (
-    <div>
-      <h1>{post.tittle}</h1>
-      <div>{ReactHtmlParser(post.content)}</div>
+    <div className="bg-black-secundary py-4 px-8 my-4 rounded-lg">
+      <div className="flex justify-end">
+        <time
+          title={publishedDateFormatted}
+          dateTime={post.publishedAt?.toISOString()} 
+          className="text-[#8d8d99] text-sm"
+        >
+          {publishedDateRelativeToNow}
+        </time>
+      </div>
+
+      <div>
+        <h1>{post.tittle}</h1>
+        <div>{ReactHtmlParser(post.content)}</div>
+      </div>
+
+      <div className="flex gap-2 text-[#8d8d99] justify-end">
+        <span>Publicado por:</span>
+        <span className="font-medium">{post.author}</span>
+      </div>
     </div>
-  )
+  );
 }
