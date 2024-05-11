bacdd502f152fc6d7f0303479dfd565cab36f5a6
rayane.melo <rayane.melo@corebiz.ag>
2024-05-11 16:04:05 -0300

    feat: logout and pagination

---
+++ frontend/src/components/CustomPagination/index.tsx
@@ -0,0 +1,31 @@
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination"
+import "./global.css"
+
+export function CustomPagination() {
+  return (
+    <Pagination >
+      <PaginationContent className="list-none">
+        <PaginationItem>
+          <PaginationPrevious href="#" />
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink href="#">1</PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationEllipsis />
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationNext href="#" />
+        </PaginationItem>
+      </PaginationContent>
+    </Pagination>
+  )
+}
--- frontend/src/layout/Header/index.tsx
+++ frontend/src/layout/Header/index.tsx
@@ -2,25 +2,33 @@
 import { CustomAvatar } from "@/components/CustomAvatar";
 import Image from "next/image";
 
-import EngSoft from '@/assets/images/engsoft.svg'
+import EngSoft from "@/assets/images/engsoft.svg";
 
 import Link from "next/link";
 import { useUserAccess } from "@/stores/user-access";
 import { useAuth } from "@/context/AuthContext";
+import { LogOutIcon } from "lucide-react";
+import { useState } from "react";
+import { twMerge } from "tailwind-merge";
 
 export function Header() {
-  const { user } = useUserAccess()
-  const { logout } = useAuth()
+  const { user } = useUserAccess();
+  const { logout } = useAuth();
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   return (
     <header className="bg-black-secundary w-full flex items-center border-b border-[#29292E] fixed top-0 left-0 right-0 h-16 z-50">
-      <div className="max-w-screen-xl mx-auto w-full px-4">
+      <div className="max-w-screen-xl mx-auto w-full px-4 flex flex-col relative">
         <div className="flex flex-row justify-between items-center">
           <Link href="/home" className="">
             {/* <Image src={Logo} alt="Logo" width={45} height={45} /> */}
             <Image src={EngSoft} alt="Logo" width={120} height={120} />
           </Link>
-
-          <CustomAvatar name={user.name} onClick={() => logout()} />
+          <CustomAvatar name={user.name} onClick={() => setIsMenuOpen(!isMenuOpen)} />
+        </div>
+        <div onClick={() => logout()} className={twMerge("absolute right-0 bg-black py-2 px-4 gap-2 flex items-center rounded top-12 cursor-pointer", isMenuOpen ? 'visible' : 'invisible')}>
+          <LogOutIcon size={18} color="#8257E5" />
+          <span className='text-sm'>Sair</span>
         </div>
       </div>
     </header>
